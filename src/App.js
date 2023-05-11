4fa354b13ea443865c6ba0cc02ab39510e05f1af
jobayerrahman <jobayer.aiub@gmail.com>
2023-05-11 19:14:52 +0600

    First commit: Golpo boilerplate ready & Test

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Golpo from "./component/Golpo";
+function App() {
+  return (
+    <div className="App">
+      <Golpo></Golpo>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Golpo.js
@@ -0,0 +1,5 @@
+export default function Golpo(){
+    return(
+        <div>Golpo</div>
+    ); 
+}
