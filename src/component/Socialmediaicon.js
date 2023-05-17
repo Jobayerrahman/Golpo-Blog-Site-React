c4388c2d30b192d60a4798501f3186f67ea43ab1
jobayerrahman <jobayer.aiub@gmail.com>
2023-05-17 12:47:04 +0600

    Commit 0006: Social Media Icon component create & Font awesome add

--- src/component/Socialmediaicon.js
+++ src/component/Socialmediaicon.js
@@ -1,11 +1,11 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {   faTwitterSquare, faYoutubeSquare, faFacebookSquare } from '@fortawesome/free-brands-svg-icons'
 export default function Socialmediaicon(){
     return(
         <div className="icon-wrapper">
-            <i>1</i>
-            <i>2</i>
-            <i>3</i>
-            <i>4</i>
-            <i>5</i>
+            <FontAwesomeIcon className="icon icon-facebook" icon={faFacebookSquare} />
+            <FontAwesomeIcon className="icon icon-youtube" icon={faYoutubeSquare} />
+            <FontAwesomeIcon className="icon icon-twitter" icon={faTwitterSquare} />
         </div>
     )
 }
