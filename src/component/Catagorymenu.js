3b0e649fbd5f9d946a2d6c3e90d6346d5f59c64e
jobayerrahman <jobayer.aiub@gmail.com>
2023-05-18 17:42:57 +0600

    Commit 0009: Catagorymenu component create

---
+++ src/component/Catagorymenu.js
@@ -0,0 +1,14 @@
+export default function Catagorymenu(){
+    return(
+        <div className="catagory-menu-wrapper">
+            <h2>Catagories: </h2>
+            <div className="catagories">
+                <span className="catagory catagory-nature mx-2">Nature</span>
+                <span className="catagory catagory-technology mx-2">Technology</span>
+                <span className="catagory catagory-entertainment mx-2">Entertainment</span>
+                <span className="catagory catagory-history mx-2">History</span>
+                <span className="catagory catagory-health mx-2">Health</span>
+            </div>
+        </div>
+    )
+}
