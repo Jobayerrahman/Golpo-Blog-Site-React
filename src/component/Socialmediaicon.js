aa3075d863b3184bd642ef188f2695d7d45def08
jobayerrahman <jobayer.aiub@gmail.com>
2023-05-30 00:54:20 +0600

    Commit 0016: Social media icon correction

--- src/component/Socialmediaicon.js
+++ src/component/Socialmediaicon.js
@@ -1,11 +1,12 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {   faTwitterSquare, faYoutubeSquare, faFacebookSquare } from '@fortawesome/free-brands-svg-icons'
+import {   faTwitterSquare, faYoutubeSquare, faFacebookSquare, faGoogleDrive } from '@fortawesome/free-brands-svg-icons'
 export default function Socialmediaicon(){
     return(
         <div className="icon-wrapper">
             <FontAwesomeIcon className="icon icon-facebook" icon={faFacebookSquare} />
             <FontAwesomeIcon className="icon icon-youtube" icon={faYoutubeSquare} />
             <FontAwesomeIcon className="icon icon-twitter" icon={faTwitterSquare} />
+            <FontAwesomeIcon className="icon icon-drive" icon={faGoogleDrive} />
         </div>
     )
 }
