69e1f7280990a5af55f7675b5173275a69389cfa
jobayerrahman <jobayer.aiub@gmail.com>
2023-07-14 22:48:32 +0600

    Commit 0044: React slick slider add on category banner

--- src/components/Cetagory/Catagories.js
+++ src/components/Cetagory/Catagories.js
@@ -1,22 +1,83 @@
+import Slider from "react-slick";
 import natureimage from "../../assets/images/mocha.webp";
 import histiryimage from "../../assets/images/history.jpg"
 import entertainment from "../../assets/images/entertainment.png"
 import technology from "../../assets/images/technology.jpg";
 import health from "../../assets/images/health.jpg";
 export default function Catagories(){
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      };
+      
     return(
         <div className="catagory-wrapper">
             <div className="catagory-card card-focus">
-                <div className="catagory-poster">
-                    <img
-                    className="catagory-image"
-                    src={histiryimage}
-                    alt="Updating Blog" 
-                    />
-                </div>
-                <div className="catagory-content">
-                    <span className="catagory catagory-History">History</span>
-                </div>
+                <Slider {...settings}>
+                    <div className="slider-item-wrapper">
+                        <div className="catagory-poster">
+                            <img
+                            className="catagory-image"
+                            src={histiryimage}
+                            alt="Updating Blog" 
+                            />
+                        </div>
+                        <div className="catagory-content">
+                            <span className="catagory catagory-History">History</span>
+                        </div>
+                    </div>
+                    <div className="slider-item-wrapper">
+                        <div className="catagory-poster">
+                            <img
+                            className="catagory-image"
+                            src={histiryimage}
+                            alt="Updating Blog" 
+                            />
+                        </div>
+                        <div className="catagory-content">
+                            <span className="catagory catagory-History">History</span>
+                        </div>
+                    </div>
+                    <div className="slider-item-wrapper">
+                        <div className="catagory-poster">
+                            <img
+                            className="catagory-image"
+                            src={histiryimage}
+                            alt="Updating Blog" 
+                            />
+                        </div>
+                        <div className="catagory-content">
+                            <span className="catagory catagory-History">History</span>
+                        </div>
+                    </div>
+                    <div className="slider-item-wrapper">
+                        <div className="catagory-poster">
+                            <img
+                            className="catagory-image"
+                            src={histiryimage}
+                            alt="Updating Blog" 
+                            />
+                        </div>
+                        <div className="catagory-content">
+                            <span className="catagory catagory-History">History</span>
+                        </div>
+                    </div>
+                    <div className="slider-item-wrapper">
+                        <div className="catagory-poster">
+                            <img
+                            className="catagory-image"
+                            src={histiryimage}
+                            alt="Updating Blog" 
+                            />
+                        </div>
+                        <div className="catagory-content">
+                            <span className="catagory catagory-History">History</span>
+                        </div>
+                    </div>
+                </Slider>
             </div>
             <div className="catagory-card">
                 <div className="catagory-poster">
@@ -39,7 +100,7 @@ export default function Catagories(){
                     />
                 </div>
                 <div className="catagory-content">
-                    <span className="catagory catagory-common">Health</span>
+                    <span className="catagory catagory-Health">Health</span>
                 </div>
             </div>
             <div className="catagory-card">
@@ -51,20 +112,72 @@ export default function Catagories(){
                     />
                 </div>
                 <div className="catagory-content">
-                    <span className="catagory catagory-common">Nature</span>
+                    <span className="catagory catagory-Nature">Nature</span>
                 </div>
             </div>
             <div className="catagory-card end-card">
-                <div className="catagory-poster">
-                    <img
-                    className="catagory-image"
-                    src={entertainment}
-                    alt="Updating Blog" 
-                    />
-                </div>
-                <div className="catagory-content">
-                    <span className="catagory catagory-common">Entertainment</span>
-                </div>
+                <Slider {...settings}>
+                    <div className="slider-item-wrapper">
+                        <div className="catagory-poster">
+                            <img
+                            className="catagory-image"
+                            src={entertainment}
+                            alt="Updating Blog" 
+                            />
+                        </div>
+                        <div className="catagory-content">
+                            <span className="catagory catagory-Entertainment">Entertainment</span>
+                        </div>
+                    </div>
+                    <div className="slider-item-wrapper">
+                        <div className="catagory-poster">
+                            <img
+                            className="catagory-image"
+                            src={entertainment}
+                            alt="Updating Blog" 
+                            />
+                        </div>
+                        <div className="catagory-content">
+                            <span className="catagory catagory-Entertainment">Entertainment</span>
+                        </div>
+                    </div>
+                    <div className="slider-item-wrapper">
+                        <div className="catagory-poster">
+                            <img
+                            className="catagory-image"
+                            src={entertainment}
+                            alt="Updating Blog" 
+                            />
+                        </div>
+                        <div className="catagory-content">
+                            <span className="catagory catagory-Entertainment">Entertainment</span>
+                        </div>
+                    </div>
+                    <div className="slider-item-wrapper">
+                        <div className="catagory-poster">
+                            <img
+                            className="catagory-image"
+                            src={entertainment}
+                            alt="Updating Blog" 
+                            />
+                        </div>
+                        <div className="catagory-content">
+                            <span className="catagory catagory-Entertainment">Entertainment</span>
+                        </div>
+                    </div>
+                    <div className="slider-item-wrapper">
+                        <div className="catagory-poster">
+                            <img
+                            className="catagory-image"
+                            src={entertainment}
+                            alt="Updating Blog" 
+                            />
+                        </div>
+                        <div className="catagory-content">
+                            <span className="catagory catagory-Entertainment">Entertainment</span>
+                        </div>
+                    </div>
+                </Slider>
             </div>
         </div>
     )
