f1a67e6f09f5161ba1785b765f9de58db8586656
jobayerrahman <jobayer.aiub@gmail.com>
2023-07-22 23:27:33 +0600

    Commit 0050: Quotes component create and data fatch with axios

---
+++ src/components/Quote/Quote.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import bannerimage from "../../assets/images/mocha.webp";
+
+function Quote({...quote}) {
+    return (
+        <div className='quote-wrapper'>
+            <div className="quote-header">
+                <img
+                    className="quote-image"
+                    src={bannerimage}
+                    alt="Commentetor" 
+                />
+                <h2>{quote.quote.author}</h2>
+            </div>
+            <div className="quote-content">
+                <p>{quote.quote.quote}</p>
+            </div>       
+        </div>
+    );
+}
+
+export default Quote;
--- src/pages/Quotespage.js
+++ src/pages/Quotespage.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import Quote from '../components/Quote/Quote';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Quotespage(props) {
+    const [quotes, setQuotes] = useState([]);
+    const quoteURL = "https://dummyjson.com/quotes";
+
+    useEffect(() => getQuote(), []);
+
+    const getQuote = () => {
+        axios.get(quoteURL).then((response) => {
+        const quotes = response.data.quotes;
+        setQuotes(quotes);
+        });
+    }
+    
     return (
-        <div>
-            <h2>Nothing here also</h2>
+        <div className='allQuote-wrapper'>
+            {quotes.map((quote) => (
+                <Quote quote={quote}/>
+            ))}
         </div>
     );
 }
