a3d0df63bc8f637ced48dfd6977707f3d7a6888a
jobayerrahman <jobayer.aiub@gmail.com>
2023-08-13 12:07:01 +0600

    Commit 0078: Blog card and details, Blog carousel, Footer responsive designing

--- src/components/Blog/Blogcarousel.js
+++ src/components/Blog/Blogcarousel.js
@@ -4,11 +4,26 @@ import 'owl.carousel/dist/assets/owl.carousel.min.css';
 import 'owl.carousel/dist/assets/owl.theme.default.min.css';
 
 function Blogcarousel({children}) {
+    const options ={
+        loop: true,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:4
+            },
+        }
+    }
     return (
         <div>
-            <OwlCarousel className='owl-theme' loop margin={10} nav>
+            <OwlCarousel className='owl-theme' loop margin={10} nav {...options}>
                 {children}
-            </OwlCarousel>;
+            </OwlCarousel>
         </div>
     );
 }
