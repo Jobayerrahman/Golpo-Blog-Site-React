31701afd19eadf69b8da038e944426d45c47a713
jobayerrahman <jobayer.aiub@gmail.com>
2023-11-13 15:17:28 +0600

    Navigation Mode Icon bug remove & transition apply

--- src/App.js
+++ src/App.js
@@ -5,24 +5,20 @@ import '../src/assets/css/cetagory.css';
 import Golpo from "./components/Golpo";
 import ThemeContext from './components/Library/ThemeContext';
 import useLocalStorage from 'use-local-storage-state';
-import { useState } from 'react';
-import { faMoon, faSun  } from '@fortawesome/free-solid-svg-icons';
 function App() {
-  const [mode, setMode] = useState(faMoon)
   const [theme,setTheme] = useLocalStorage('theme'? 'dark' : 'light')
 
   const switchTheme = () =>{
-    if(mode === faMoon){
-      setMode(faSun);
+    if(theme === 'light'){
       setTheme('dark');
     }else{
-      setMode(faMoon);
       setTheme('light');
     }
   }
+
   return (
     <div className="App" data-theme={theme}>
-      <ThemeContext.Provider value={{ mode: mode, switchTheme: switchTheme }}>
+      <ThemeContext.Provider value={{ theme: theme, switchTheme: switchTheme }}>
         <Golpo/>
       </ThemeContext.Provider>
     </div>
--- src/components/Navigation/Navigationtheme.js
+++ src/components/Navigation/Navigationtheme.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '../../assets/css/style.css';
+import React, { useContext }  from 'react';
 import ThemeContext from '../Library/ThemeContext';
-import { useContext } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon, faSun  } from '@fortawesome/free-solid-svg-icons';
+
 function Navigationtheme() {
     const context = useContext(ThemeContext);
-    const { mode, switchTheme } = context;
-    return (
-        <div className='navigation-theme'>
-            {/* <ThemeContext.Consumer>
-                {({mode, switchTheme}) =>  */}
-            <FontAwesomeIcon 
-            onClick={switchTheme} 
-            className='navigation-icon' 
-            icon={mode} />
-            {/* }
-            </ThemeContext.Consumer> */}
-        </div>
-    );
+    const { theme, switchTheme } = context;
+
+    if(theme === 'light'){
+        return (
+            <div className='navigation-theme'>
+                <FontAwesomeIcon onClick={switchTheme} className='navigation-icon' icon={faMoon} />
+            </div>
+        );
+    }else{
+        return (
+            <div className='navigation-theme'>
+                <FontAwesomeIcon onClick={switchTheme} className='navigation-icon' icon={faSun} />
+            </div>
+        );
+    }
 }
 
 export default Navigationtheme;
