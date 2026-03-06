4ff55b87cbdb4e82ccbd7637e2b2c50c8007395e
jobayerrahman <jobayer.aiub@gmail.com>
2026-03-07 01:08:13 +0600

    Last commit of legacy code of golpo.

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,15 +1,15 @@
 import '../../assets/css/navigation.css';
+
+import { NavLink } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import Container from 'react-bootstrap/Container';
-import { NavLink } from "react-router-dom";
+
 import Navigationlang from './Navigationlang';
 import Navigationsearch from './Navigationsearch';
 import Navigationtheme from './Navigationtheme';
 import Navigationuser from './Navigationuser';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {  faSquarePlus  } from '@fortawesome/free-solid-svg-icons';
 
 export default function Navigation(){
     return(
