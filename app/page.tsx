48889e25cbddcfb42a32324d6270ebc3302738bf
jobayerrahman <jobayer.aiub@gmail.com>
2026-03-07 01:51:21 +0600

    New Version Project Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
@@ -58,6 +58,7 @@ export default function Home() {
           >
             Documentation
           </a>
+          <Button>Click Me</Button>
         </div>
       </main>
     </div>
